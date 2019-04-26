c8b8c7f9e24bebe9a6f2a203adaa3d31274e81d6
Gabrieli Barbosa <ifc.gabrieli.barbosa@gmail.com>
2019-04-26 17:02:24 -0300

    Ajustando botões e layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +16,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
+    BarcodeScanner,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  options: BarcodeScannerOptions;
+  encodeText: string = '';
+  encodedData: any={};
+  scannedData: any={};
 
+  constructor(private scanner: BarcodeScanner) { }
+
+  scan(){
+    this.options={
+      prompt:"Scan your barcode"
+    }
+    this.scanner.scan().then(data => {
+      this.scannedData = data;
+       alert(data);
+     }).catch(err => {
+         console.log('Error', err);
+     });
+  }
+
+  encode(){
+    this.scanner.encode(this.scanner.Encode.TEXT_TYPE, this.encodeText).then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+     }).catch(err => {
+         console.log('Error', err);
+     });
+  }
 }
